898caca4c3001b3aa6920129acbb9ba09cad68c4
Michael Sarchet <mikesarchet@gmail.com>
2020-12-13 11:45:17 -0800

    Working hashes

---
+++ Panzer.cpp
@@ -0,0 +1,777 @@
+// Panzer.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "constants.h"
+#include <chrono>
+
+
+void fen_to_board(const std::string& fen, board* board);
+void make_move(const move& move, board* board);
+void unmake_move(const move& move, board* board);
+bool isAttacked(const square& square, board* board);
+move build_move(square from_square, square to_square, piece from, piece captured, char flags, board* b);
+int generate_moves(move moves[], board* board);
+void print_move(move m);
+int perft(int depth, board* b, move moves[256]);
+int sum_perft_nodes(board* b, int depth);
+
+void hash_start_board(board* b);
+
+int main(int argc, char *argv[])
+{
+	fill_hash_lookup();
+	// Using time point and system_clock 
+    std::chrono::time_point<std::chrono::steady_clock> start, end; 
+  
+
+	std::cout << "Running";
+	board* b = new board;
+	fen_to_board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR", b);
+	b->side_to_move = WHITE;
+	hash_start_board(b);
+	int depth = 4;
+	if (argc > 1) 
+	{
+		depth = argv[1][0] - '0';
+	}
+
+	std::cout << "Peft depth " << depth << "\n";
+	std::cout << "Starting Hash" << b->zorbist << "\n";
+	hashed_board starting_hash;
+	move moves[256];
+	start = std::chrono::high_resolution_clock::now();
+
+	int nodes = perft(depth, b, moves);
+	end = std::chrono::high_resolution_clock::now();
+	for (int i = 0; i < nodes; i++) 
+	{
+		starting_hash.moves[i] = moves[i];
+	}
+	starting_hash.move_count = nodes;
+	std::cout << "Ending Hash" << b->zorbist << "\n";
+	hashed_boards[b->zorbist] = starting_hash;
+	int total_moves = sum_perft_nodes(b, depth);
+
+    std::chrono::duration<double> elapsed_seconds = end - start; 
+	std::cout << "Time:" << elapsed_seconds.count();
+	std::cout << "\n";
+	std::cout << "Perft" << depth << " " << total_moves;
+	std::cout << "Moves PS: " <<total_moves / elapsed_seconds.count();
+	std::cin.get();
+}
+int sum_perft_nodes(board* b, int depth)
+{
+	int sum = 0;
+
+	hashed_board hashed = hashed_boards[b->zorbist];
+
+	for (int i = 0; i < hashed.move_count; i++) 
+	{
+		if (depth == 1) 
+		{
+			sum++;
+		}
+		else
+		{
+			make_move(hashed.moves[i], b);
+			sum += sum_perft_nodes(b, depth - 1);
+			unmake_move(hashed.moves[i], b);
+		}
+	}
+
+	return sum;
+}
+void hash_start_board(board* b) 
+{
+	for (square s = 0; s < 128; s++)
+	{
+		if (IS_SQ(s))
+		{
+			if (b->pieces[s] != NONE)
+			{
+				b->zorbist ^= zorbist_hash_lookup[s][b->pieces[s]][b->colors[s]];
+			}
+		}
+	}
+}
+
+int perft(int depth, board* b, move*  moves[256]) 
+{
+	int king_index = b->side_to_move == WHITE ? b->white_king : b->black_king;
+	int move_count;
+
+	if (hashed_boards.count(b->zorbist))
+	{
+		hashed_board board_from_hash = hashed_boards[b->zorbist];
+		// we will hash boards and not hash moves for them because they are 
+		// at the last ply
+		if (board_from_hash.move_count != 0 or board_from_hash.isCheckmate)
+		{
+			// moves = board_from_hash.moves;
+			for (int i = 0; i < board_from_hash.move_count; i++)
+			{
+				moves[i] = board_from_hash.moves[i];
+			}
+			return board_from_hash.move_count;
+		}
+	}
+
+	move raw_moves[256];
+	move_count = generate_moves(raw_moves, b);
+	int legal_move_count = 0;
+	
+	for (int i = 0; i < move_count; i++)
+	{
+		make_move(raw_moves[i], b);
+		hashed_board hashed_board;
+		hashed_board.hash = b->zorbist;
+
+
+		if (!isAttacked(square(king_index), b))
+		{
+			moves[legal_move_count] = raw_moves[i];
+			if (depth != 1) 
+			{
+
+				move deep_moves[256];
+				hashed_board.move_count = perft(depth - 1, b, deep_moves);
+
+				
+				for (int j = 0; j < hashed_board.move_count; j++) 
+				{
+					hashed_board.moves[j] = deep_moves[j];
+				}
+			}
+
+			legal_move_count++;
+		}
+
+		hashed_boards[b->zorbist] = hashed_board;
+		unmake_move(raw_moves[i], b);
+	}
+	return legal_move_count;
+}
+
+void print_move(move m) 
+{
+	std::cout << square_names[m.from] << square_names[m.to];
+}
+
+void make_move(const move& m, board* board) 
+{
+	if (m.capture != NONE) 
+	{
+		if (board->colors[m.to] == WHITE) 
+		{
+			board->white_pieces.erase(m.to);
+			board->black_pieces.erase(m.from);
+			board->black_pieces.insert(m.to);
+		}
+		else
+		{
+			board->black_pieces.erase(m.to);
+			board->white_pieces.erase(m.from);
+			board->white_pieces.insert(m.to);
+		}
+	}
+
+	board->zorbist ^= color_hash;
+	// remove from piece
+	board->zorbist ^= zorbist_hash_lookup[m.from][board->pieces[m.from]][board->colors[m.from]];
+	board->pieces[m.from] = NONE;
+	board->colors[m.from] = NO_COLOR;
+
+	// clear to piece
+	if (m.capture)
+	{
+		board->zorbist ^= zorbist_hash_lookup[m.to][board->pieces[m.to]][board->colors[m.to]];
+	}
+
+	// add moved piece
+	board->colors[m.to] = board->side_to_move;
+	board->pieces[m.to] = m.piece_to;
+	board->zorbist ^= zorbist_hash_lookup[m.to][m.piece_to][board->colors[m.to]];
+
+	// adjust castling flags
+	if (board->castle_moves) 
+	{
+		if (m.from == E1) 
+		{
+			board->castle_moves ^= (WHITEK | WHITEQ);
+		}
+
+		if (m.from == A1)
+		{
+			board->castle_moves ^= WHITEQ;
+		}
+
+		if (m.from == H1)
+		{
+			board->castle_moves ^= WHITEK;
+		}
+
+		if (m.from == E8) 
+		{
+			board->castle_moves ^= (BLACKK | BLACKQ);
+		}
+
+		if (m.from == A8)
+		{
+			board->castle_moves ^= BLACKQ;
+		}
+
+		if (m.from == H8)
+		{
+			board->castle_moves ^= BLACKK;
+		}
+	}
+
+	board->ep_square = m.ep;
+	board->side_to_move = board->side_to_move == BLACK ? WHITE : BLACK;
+}
+
+void unmake_move(const move& m, board* board) 
+{
+	board->side_to_move = board->side_to_move == BLACK ? WHITE : BLACK;
+	// remove to piece
+	board->zorbist ^= zorbist_hash_lookup[m.to][board->pieces[m.to]][board->colors[m.to]];
+	board->pieces[m.to] = m.capture;
+	if (m.capture) 
+	{
+		board->colors[m.to] = board->side_to_move == WHITE ? BLACK : WHITE;
+		// add back captured piece
+		board->zorbist ^= zorbist_hash_lookup[m.to][board->pieces[m.to]][board->colors[m.to]];
+	}
+	else
+	{
+		board->colors[m.to] = NO_COLOR;
+	}
+
+	// add back from piece
+	board->pieces[m.from] = m.piece_from;
+	board->colors[m.from] = board->side_to_move;
+	board->zorbist ^= zorbist_hash_lookup[m.from][board->pieces[m.from]][board->colors[m.from]];
+	board->zorbist ^= color_hash;
+
+	if (board->side_to_move == WHITE)
+	{
+		board->white_pieces.insert(m.from);
+		board->white_pieces.erase(m.to);
+		if (m.capture)
+		{
+			board->black_pieces.insert(m.to);
+		}
+	}
+	else
+	{
+		board->black_pieces.insert(m.from);
+		board->black_pieces.erase(m.to);
+		if (m.capture)
+		{
+			board->white_pieces.insert(m.to);
+		}
+	}
+	// adjust castling flags
+	// do things here if move is castle
+	if (m.castle) 
+	{
+		// check m.to square to revert a castle
+	}
+
+	//update_hash(m, board);
+	board->ep_square = m.ep;
+}
+
+bool isAttacked(const square& square, board* board) 
+{
+	color moving_color = board->colors[square];
+	color attack_color = moving_color;
+
+	if (moving_color == WHITE) 
+	{
+		attack_color = BLACK;
+	}
+	else if (moving_color == BLACK) 
+	{
+		attack_color = WHITE;
+	}
+
+	for (int ray: knight_moves) 
+	{
+		const int target = square + ray;
+		
+		if (IS_SQ(target) and board->colors[target] == attack_color) 
+		{
+			if (board->pieces[target] == KNIGHT) 
+			{
+				return true;
+			}
+		}
+	}
+
+	for (int ray : rays) 
+	{
+		int target = square + ray;
+		bool in_pawn_range = true;
+		while (IS_SQ(target))
+		{
+			if (board->colors[target] == moving_color)
+			{
+				break;
+			}
+			else if (board->colors[target] == attack_color)
+			{
+				piece target_piece = board->pieces[target];
+				if (in_pawn_range && target_piece == PAWN) 
+				{	
+					if (attack_color == WHITE 
+						&& (ray == NW || ray == NE)) 
+					{
+						return true;
+					}
+
+					if (attack_color == BLACK
+						&& (ray == SW || ray == SE)) 
+					{
+						return true;
+					}
+				}
+
+				if (in_pawn_range && target_piece == KING) 
+				{
+					return true;
+				}
+
+				if (ray == NW || ray == NE || ray == SE || ray == SW)
+				{
+					if (target_piece == (BISHOP | QUEEN))
+					{
+						return true;
+					}
+				}
+				else 
+				{
+					if (target_piece == (QUEEN | ROOK))
+					{
+						return true;
+					}
+				}
+			}
+
+			// move along the ray
+			target += ray;
+		}
+	}
+
+	return false;
+}
+
+
+int generate_moves(move moves[], board* board)
+{
+	int move_count = 0;
+	char castle_moves = board->castle_moves;
+	color same_side = board->side_to_move;
+	color other_side = board->side_to_move == WHITE ? BLACK : WHITE;
+
+	if (board->side_to_move == WHITE)
+	{
+		if (castle_moves & WHITEK)
+		{
+			if (board->pieces[B1] == NONE and board->pieces[C1] == NONE and board->pieces[D1] == NONE)
+			{
+				if (!isAttacked(C1, board) and !isAttacked(D1, board))
+				{
+					moves[move_count] = build_move(E1, C1, KING, NONE, CASTLE, board);
+					move_count++;
+				}
+			}
+		}
+		if (castle_moves & WHITEQ)
+		{
+			if (board->pieces[F1] == NONE and board->pieces[G1] == NONE)
+			{
+				if (!isAttacked(F1, board) and !isAttacked(G1, board))
+				{
+					moves[move_count] = build_move(E1, G1, KING, NONE, CASTLE, board);
+					move_count++;
+				}
+			}
+		}
+
+	}
+	else if (board->side_to_move == BLACK)
+	{
+		if (castle_moves & BLACKK)
+		{
+			if (board->pieces[B8] == NONE and board->pieces[C8] == NONE and board->pieces[D8] == NONE)
+			{
+				if (!isAttacked(C8, board) and !isAttacked(D8, board))
+				{
+					moves[move_count] = build_move(E8, C8, KING, NONE, CASTLE, board);
+					move_count++;
+				}
+			}
+		}
+		if (castle_moves & BLACKQ)
+		{
+			if (board->pieces[F8] == NONE and board->pieces[G8] == NONE)
+			{
+				if (!isAttacked(F8, board) and !isAttacked(G8, board))
+				{
+					moves[move_count] = build_move(E8, G8, KING, NONE, CASTLE, board);
+					move_count++;
+				}
+			}
+		}
+	}
+
+	std::set<int> same_side_pieces;
+	if (board->side_to_move == WHITE)
+	{
+		same_side_pieces = board->white_pieces;
+	}
+	else
+	{
+		same_side_pieces = board->black_pieces;
+	}
+
+	for (int square_index : same_side_pieces)
+	{
+		piece current_piece = board->pieces[square_index];
+		int current_target = square_index;
+		bool slides = (current_piece & (PAWN | KNIGHT | KING)) != current_piece;
+
+		int move_rays[8] = {0,0,0,0,0,0,0};
+		int ray_count = 8;
+		if (current_piece & BISHOP) 
+		{
+			move_rays[0] = NW;
+			move_rays[1] = NE;
+			move_rays[2] = SW;
+			move_rays[3] = SE;
+			ray_count = 4;
+		}
+
+		if (current_piece & (KING | QUEEN))
+		{
+			for (int i = 0; i < 8; i++) 
+			{
+				move_rays[i] = rays[i];
+			}
+		}
+
+		if (current_piece & ROOK) 
+		{
+			move_rays[0] = N;
+			move_rays[1] = S;
+			move_rays[2] = W;
+			move_rays[3] = E;
+			ray_count = 4;
+		}
+
+		if (current_piece & KNIGHT) 
+		{
+			for (int i = 0; i < 8; i++) 
+			{
+				move_rays[i] = knight_moves[i];
+			}
+
+		}
+		if (current_piece & PAWN) 
+		{
+			if (board->side_to_move == WHITE) 
+			{
+				if (square_index / 16 == 1) 
+				{
+					current_target = square_index + NN;
+					if (IS_SQ(current_target) and board->pieces[current_target] == NONE) 
+					{
+						moves[move_count] = build_move(square(square_index), square(current_target), PAWN, NONE, NO_FLAGS, board);
+						move_count++;
+					}
+				}
+
+				current_target = square_index + N;
+				if (IS_SQ(current_target) && board->pieces[current_target] == NONE) 
+				{
+					if (current_target / 16 == 7) 
+					{
+						for (piece promote : {QUEEN, ROOK, KNIGHT, BISHOP}) 
+						{
+							moves[move_count] = build_move(square(square_index), square(current_target), PAWN, NONE, PROMOTION, board);
+							moves[move_count].piece_to = promote;
+							move_count++;
+						}
+					}
+					else 
+					{
+						moves[move_count] = build_move(square(square_index), square(current_target), PAWN, NONE, NO_FLAGS, board);
+						move_count++;
+					}
+				}
+
+				int pawn_captures[] = { square_index + NW, square_index + NE };
+				for (int capture_index: pawn_captures) 
+				{
+					if (IS_SQ(capture_index) and board->pieces[capture_index] != NONE && board->colors[capture_index] != board->side_to_move) 
+					{
+						if (capture_index / 16 == 7) 
+						{
+							for (piece promote : {QUEEN, ROOK, KNIGHT, BISHOP}) 
+							{
+								moves[move_count] = build_move(square(square_index), square(capture_index), PAWN, board->pieces[capture_index], (CAPTURE | PROMOTION), board);
+								moves[move_count].piece_to = promote;
+								move_count++;
+							}
+						}
+						else 
+						{
+							moves[move_count] = build_move(square(square_index), square(capture_index), PAWN, board->pieces[capture_index], CAPTURE, board);
+							move_count++;
+						}
+					}
+				}
+				
+			}
+
+			if (board->side_to_move == BLACK) 
+			{
+				if (square_index / 16 == 6) 
+				{
+					current_target = square_index + SS;
+					if (IS_SQ(current_target) and board->pieces[current_target] == NONE) 
+					{
+						moves[move_count] = build_move(square(square_index), square(current_target), PAWN, NONE, NO_FLAGS, board);
+						move_count++;
+					}
+				}
+				current_target = square_index + S;
+				if (IS_SQ(current_target) and board->pieces[current_target] == NONE) 
+				{
+					if (current_target / 16 == 0) 
+					{
+						for (piece promote : {QUEEN, ROOK, KNIGHT, BISHOP}) 
+						{
+							moves[move_count] = build_move(square(square_index), square(current_target), PAWN, NONE, PROMOTION, board);
+							moves[move_count].piece_to = promote;
+							move_count++;
+						}
+					}
+					else 
+					{
+						moves[move_count] = build_move(square(square_index), square(current_target), PAWN, NONE, NO_FLAGS, board);
+						move_count++;
+					}
+				}
+
+				int pawn_captures[] = { square_index + SW, square_index + SE };
+				for (int capture_index: pawn_captures) 
+				{
+					if (IS_SQ(capture_index) and board->pieces[capture_index] != NONE && board->colors[capture_index] != board->side_to_move) 
+					{
+						if (capture_index/ 16 == 7) 
+						{
+							for (piece promote : {QUEEN, ROOK, KNIGHT, BISHOP}) 
+							{
+								moves[move_count] = build_move(square(square_index), square(capture_index), PAWN, board->pieces[capture_index], (CAPTURE | PROMOTION), board);
+								moves[move_count].piece_to = promote;
+								move_count++;
+							}
+						}
+						else 
+						{
+							moves[move_count] = build_move(square(square_index), square(capture_index), PAWN, board->pieces[capture_index], CAPTURE, board);
+							move_count++;
+						}
+					}
+				}
+			}
+		}
+		else 
+		{
+			for (int ray_index = 0; ray_index < ray_count; ray_index++) 
+			{
+				int dir = move_rays[ray_index];
+				current_target = square_index + dir;
+				while (IS_SQ(current_target)) 
+				{
+					if (board->colors[current_target] == board->side_to_move) 
+					{
+						break;
+					}
+
+					if (board->pieces[current_target] == NONE) 
+					{
+						moves[move_count] = build_move(square(square_index), square(current_target), current_piece, NONE, NO_FLAGS, board);
+						move_count++;
+					}
+					else
+					{
+						moves[move_count] = build_move(square(square_index), square(current_target), current_piece, board->pieces[current_target], CAPTURE, board);
+						move_count++;
+						break;
+					}
+
+					if (!slides)
+					{
+						break;
+					}
+
+					current_target += dir;
+				}
+			}
+
+		}
+	}
+
+	return move_count;
+}
+
+move build_move(square from_square, square to_square, piece from, piece captured, char flags, board* b) 
+{
+	move m = move();
+	m.from = from_square;
+	m.to = to_square;
+	m.capture = captured;
+	m.piece_from = from;
+	m.piece_to = from;
+	m.flags = flags;
+	m.ply = b->ply;
+	m.castle = b->castle_moves != 0;
+	m.ep = b->ep_square;
+	return m;
+}
+
+void fen_to_board(const std::string& fen, board* board)
+{
+	int index = 0;
+	bool board_done = false;
+	for (int index = 0; index < 128; index++) 
+	{
+		board->pieces[index] = NONE;
+		board->colors[index] = NO_COLOR;
+	}
+	for (char const& c : fen) 
+	{
+		if (board_done)
+		{
+			break;
+		}
+
+		switch (c) {
+			case 'r':
+				board->pieces[index] = ROOK;
+				board->colors[index] = WHITE;
+				index++;
+				break;
+			case 'n':
+				board->pieces[index] = KNIGHT;
+				board->colors[index] = WHITE;
+				index++;
+				break;
+			case 'b':
+				board->pieces[index] = BISHOP;
+				board->colors[index] = WHITE;
+				index++;
+				break;
+			case 'q':
+				board->pieces[index] = QUEEN;
+				board->colors[index] = WHITE;
+				index++;
+				break;
+			case 'k':
+				board->pieces[index] = KING;
+				board->colors[index] = WHITE;
+				index++;
+				break;
+			case 'p':
+				board->pieces[index] = PAWN;
+				board->colors[index] = WHITE;
+				index++;
+				break;
+			case 'R':
+				board->pieces[index] = ROOK;
+				board->colors[index] = BLACK;
+				index++;
+				break;
+			case 'N':
+				board->pieces[index] = KNIGHT;
+				board->colors[index] = BLACK;
+				index++;
+				break;
+			case 'B':
+				board->pieces[index] = BISHOP;
+				board->colors[index] = BLACK;
+				index++;
+				break;
+			case 'Q':
+				board->pieces[index] = QUEEN;
+				board->colors[index] = BLACK;
+				index++;
+				break;
+			case 'K':
+				board->pieces[index] = KING;
+				board->colors[index] = BLACK;
+				index++;
+				break;
+			case 'P':
+				board->pieces[index] = PAWN;
+				board->colors[index] = BLACK;
+				index++;
+				break;
+			case '/':
+				index += 8;
+				break;
+			case '1':
+				index += 1;
+				break;
+			case '2':
+				index += 2;
+				break;
+			case '3':
+				index += 3;
+				break;
+			case '4':
+				index += 4;
+				break;
+			case '5':
+				index += 5;
+				break;
+			case '6':
+				index += 6;
+				break;
+			case '7':
+				index += 7;
+				break;
+			case '8':
+				index += 8;
+				break;
+			case ' ':
+				board_done = true;
+				break;
+		}
+	}
+
+	for (int index = 0; index < 128; index++) 
+	{
+		if (board->colors[index] == WHITE) 
+		{
+			if (board->pieces[index] == KING) 
+			{
+				board->white_king = square(index);
+			}
+
+			board->white_pieces.insert(index);
+		}
+		else if (board->colors[index] == BLACK) 
+		{
+			board->black_pieces.insert(index);
+			if (board->pieces[index] == KING) 
+			{
+				board->black_king = square(index);
+			}
+		}
+	}
+}
---
+++ constants.h
@@ -0,0 +1,363 @@
+#pragma once
+#include <set>
+#include <map>
+#include <stdlib.h>
+#include <random>
+#include <cmath>
+
+#define IS_SQ(x)  ( (x) & 0x88 ) ? (0) : (1)
+
+constexpr auto STARTFEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+
+typedef unsigned __int8 square;
+typedef unsigned __int8 piece;
+typedef unsigned __int8 color;
+typedef unsigned __int8 move_flags;
+typedef unsigned __int64 hash;
+typedef unsigned __int8 castle_flags;
+
+
+const square A1 = 1;
+const square B1 = 2;
+const square C1 = 3;
+const square D1 = 4;
+const square E1 = 5;
+const square F1 = 6;
+const square G1 = 7;
+const square H1 = 8;
+const square A2 = A1 + 16;
+const square B2 = A2 + 1;
+const square C2 = B2 + 1;
+const square D2 = C2 + 1;
+const square E2 = D2 + 1;
+const square F2 = E2 + 1;
+const square G2 = F2 + 1;
+const square H2 = G2 + 1;
+const square A3 = A2 + 16;
+const square B3 = A3 + 1;
+const square C3 = B3 + 1;
+const square D3 = C3 + 1;
+const square E3 = D3 + 1;
+const square F3 = E3 + 1;
+const square G3 = F3 + 1;
+const square H3 = G3 + 1;
+const square A4 = A3 + 16;
+const square B4 = A4 + 1;
+const square C4 = B4 + 1;
+const square D4 = C4 + 1;
+const square E4 = D4 + 1;
+const square F4 = E4 + 1;
+const square G4 = F4 + 1;
+const square H4 = G4 + 1;
+const square A5 = A4 + 16;
+const square B5 = A5 + 1;
+const square C5 = B5 + 1;
+const square D5 = C5 + 1;
+const square E5 = D5 + 1;
+const square F5 = E5 + 1;
+const square G5 = F5 + 1;
+const square H5 = G5 + 1;
+const square A6 = A5 + 16;
+const square B6 = A5 + 1;
+const square C6 = B6 + 1;
+const square D6 = C6 + 1;
+const square E6 = D6 + 1;
+const square F6 = E6 + 1;
+const square G6 = F6 + 1;
+const square H6 = G6 + 1;
+const square A7 = A6 + 16;
+const square B7 = A6 + 1;
+const square C7 = B7 + 1;
+const square D7 = C7 + 1;
+const square E7 = D7 + 1;
+const square F7 = E7 + 1;
+const square G7 = F7 + 1;
+const square H7 = G7 + 1;
+const square A8 = A7 + 16;
+const square B8 = A8 + 1;
+const square C8 = B8 + 1;
+const square D8 = C8 + 1;
+const square E8 = D8 + 1;
+const square F8 = E8 + 1;
+const square G8 = F8 + 1;
+const square H8 = G8 + 1;
+const square EMPTY = H8 + 1;
+
+const piece	NONE = 0;
+const piece	PAWN = 2;
+const piece	ROOK = 4;
+const piece	BISHOP = 8;
+const piece	KNIGHT = 16;
+const piece	QUEEN = 32;
+const piece	KING = 64;
+
+const color	WHITE = 0;
+const color	BLACK = 1;
+const color	NO_COLOR = 2;
+
+
+const move_flags NO_FLAGS = 0;
+const move_flags CASTLE = 2;
+const move_flags EP = 4;
+const move_flags PROMOTION = 8;
+const move_flags CAPTURE = 16;
+
+struct move
+{
+	char id = 0;
+	square from = EMPTY;
+	square to = EMPTY;
+	piece piece_from = NONE;
+	piece piece_to = NONE;
+	piece capture = NONE;
+	bool castle = false;
+	char flags = NO_FLAGS;
+	int ply = 0;
+	int score = 0;
+	square ep = EMPTY;
+};
+
+
+struct board
+{
+	piece pieces[128] = { 0 };
+	color colors[128] = { 0 };
+	std::set<int> white_pieces;
+	std::set<int> black_pieces;
+	color side_to_move = WHITE;
+	char castle_moves = (1 | 2 | 4 | 8); // 1K 2 Q 4 k 4 q
+	square ep_square = EMPTY;
+	__int8 ply = 0;
+	hash zorbist = 0;
+	square black_king = E8;
+	square white_king = E1;
+};
+
+
+const castle_flags WHITEK = 1;
+const castle_flags WHITEQ = 2;
+const castle_flags BLACKK = 4;
+const castle_flags BLACKQ = 8;
+
+const int NW = 16 - 1;
+const int N = 16;
+const int NE = 16 + 1;
+const int E = 1;
+const int SE = -16 + 1;
+const int S = -16;
+const int SW = -16 - 1;
+const int W = -1;
+const int NN = 32;
+const int SS = -32;
+
+// left, up, right, down
+const int knight_moves[8] 
+{
+
+	-2 + N, // left up
+	-2 + S, // left down
+	NN - 1, // up left
+	NN + 1,  // up right
+	2 + N, // right up 
+	2 + S,  // right down
+	-1 + SS,  // down left
+	1 +SS // down right
+};
+
+const int rays [] =
+{
+	NW,
+	W,
+	NE,
+	E,
+	SE,
+	S,
+	SW,
+	W
+};
+
+#pragma region LOOKUPS
+
+const std::string square_names[]
+{
+	"A1",
+	"B1",
+	"C1",
+	"D1",
+	"E1",
+	"F1",
+	"G1",
+	"H1",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"A2",
+	"B2",
+	"C2",
+	"D2",
+	"E2",
+	"F2",
+	"G2",
+	"H2",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"A3",
+	"B3",
+	"C3",
+	"D3",
+	"E3",
+	"F3",
+	"G3",
+	"H3",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"A4",
+	"B4",
+	"C4",
+	"D4",
+	"E4",
+	"F4",
+	"G4",
+	"H4",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"A5",
+	"B5",
+	"C5",
+	"D5",
+	"E5",
+	"F5",
+	"G5",
+	"H5",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"A6",
+	"B6",
+	"C6",
+	"D6",
+	"E6",
+	"F6",
+	"G6",
+	"H6",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"A7",
+	"B7",
+	"C7",
+	"D7",
+	"E7",
+	"F7",
+	"G7",
+	"H7",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"A8",
+	"B8",
+	"C8",
+	"D8",
+	"E8",
+	"F8",
+	"G8",
+	"H8",
+	"EMPTY"
+};
+
+std::string piece_name(piece p)
+{
+	switch (p) 
+	{
+		case NONE: return "";
+		case PAWN: return "P";
+		case ROOK: return "R";
+		case BISHOP: return "B";
+		case KNIGHT: return "N";
+		case QUEEN: return "Q ";
+		case KING: return "K";
+	}
+};
+
+const std::string color_names[]
+{
+	"WHITE",
+	"BLACK",
+	"NO_COLOR"
+};
+
+struct hashed_board
+{
+	__int64 hash = 0;
+	move* moves[256] = { 0 };
+	int move_count = 0;
+	int score = 0;
+	bool isCheckmate = false;:w
+};
+
+std::map<square, std::map<piece, std::map<color, hash>>>  zorbist_hash_lookup;
+hash color_hash;
+std::map<hash, hashed_board> hashed_boards;
+
+void fill_hash_lookup()
+{
+	std::random_device rd;
+	std::mt19937_64 e2(rd());
+	std::uniform_int_distribution<unsigned long long> dist(1, -1LL);
+
+	for (square s = 0; s < 128; s++)
+	{
+		if (IS_SQ(s)) 
+		{
+			for (piece p : { PAWN, ROOK, KNIGHT, BISHOP, QUEEN, KING, NONE})
+			{
+				for (color c : { WHITE, BLACK, NO_COLOR})
+				{
+					zorbist_hash_lookup[s][p][c] = dist(e2);
+				}
+			}
+		}
+	}
+
+	color_hash = dist(e2);
+}
+#pragma endregion LOOKUPS
+
